569491c4f2d5b4493ad1ead3c9947f8b9f6220a1
Stephen Franklin <stephenLfranklin@gmail.com>
2012-05-18 07:17:46 -0500

    more classes

--- superSubClasses/superSubClasses/main.m
+++ superSubClasses/superSubClasses/main.m
@@ -14,10 +14,13 @@
 
 @interface ClassA: NSObject 
 {
-	int x; 
+	int x;  // will be inherited by subclasses
 }
 
--(void) initVar; 
+-(void) initVar;
+-(void) printVar;   
+// added the printVar method here, even though it was already in ClassB 
+// because inheritance is not bidirectional. 
 @end
 
 @implementation ClassA 
@@ -25,31 +28,101 @@
 {
 	x = 100; 
 }
+-(void) printVar
+{
+	NSLog (@"x = %i", x);
+}
+
 @end
 
 // Class B declaration and definition
 
 @interface ClassB: ClassA 
--(void) printVar;
+-(void) initVar;  // added method overrides the parent (ClassA) method
+				  //-(void) printVar;
 @end
 
-@implementation ClassB 
+@implementation ClassB
+-(void) initVar
+{
+	x = 200;
+}
 -(void) printVar
 {
 	NSLog (@"x = %i", x); 
 }
 @end
 
+@interface ClassB2: ClassA
+-(void) negVar;
+@end
+
+@implementation ClassB2
+-(void) negVar 
+{
+	x *= -1; 
+}
+@end
+
+@interface ClassC: ClassB
+-(void) printVar;
+@end
+
+@implementation ClassC
+-(void) printVar;
+{
+	NSLog (@"ClassC says x is still %i, bitches.", x);
+}
+@end
+
+
+int main (int argc, char * argv[])
+{
+
+	@autoreleasepool {
+		ClassA *a = [[ClassA alloc] init];
+		ClassB *b = [[ClassB alloc] init];
+		ClassB2 *b2 = [[ClassB2 alloc] init];
+		ClassC *c = [[ClassC alloc] init];
+		
+		[a initVar]; // uses ClassA method		
+		[a printVar]; // reveal value of x; 
+
+		[b initVar]; // uses overriding ClassB method		
+		[b printVar]; // reveal value of x; 
+		
+		[b2 initVar];
+		[b2 negVar];
+		[b2 printVar];
+		
+		[c initVar];
+		[c printVar];
+		
+		
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+/*
 int main(int argc, const char * argv[])
 {
 
 	@autoreleasepool {
 		ClassB *b = [[ClassB alloc] init];
 		
-		[b initVar]; // will use inherited method		
+		//		[b initVar]; // will use inherited method		
+		[b initVar]; // will use overriding method in ClassB		
 		[b printVar]; // reveal value of x; 
 	}
 		
 		return 0;
 }
-
+*/
